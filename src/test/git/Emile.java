202a505ea547dd51bb976f2dd15c10dc5ac232ea
ekane3 <ekaneekane3@gmail.com>
2020-04-25 20:26:26 +0200

    Ajout de la classe Emile

---
+++ src/test/git/Emile.java
@@ -0,0 +1,24 @@
+package test.git;
+
+public class Emile {
+	public void bonjour() {
+		System.out.println( "Bonjoour" );
+	}
+	
+	
+	private String[] adresses = {
+			"14 Rue Mozart, Paris",
+			"77 Rue Picasso, Toulouse", 
+			"53 Rue des fleurs, Limoges",
+	};
+
+	
+	public String getAdresse( int i ) {
+
+		if ( 0 <= i && i < adresses.length ) {
+			return adresses[i];
+		} else {
+			return null;
+		}
+	}
+}
