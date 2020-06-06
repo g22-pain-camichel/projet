27d9f55d2c5aeb2fd4ccdfa1f3fbb9fbd51f75ab
ekane3 <ekaneekane3@gmail.com>
2020-06-06 15:10:39 +0200

    Probleme de l'affichage de la vue résolue

--- src/projet/view/epreuveadmin/EpreuveAdminController.java
+++ src/projet/view/epreuveadmin/EpreuveAdminController.java
@@ -1,5 +1,22 @@
 package projet.view.epreuveadmin;
 
-public class EpreuveAdminController {
+import javax.inject.Inject;
 
+import javafx.fxml.FXML;
+import jfox.javafx.view.IManagerGui;
+import projet.view.EnumView;
+
+public class EpreuveAdminController {
+	@Inject
+	private IManagerGui		managerGui;
+
+	@FXML
+	public void doAccueil() {
+		managerGui.showView(EnumView.Accueil);
+	}
+	
+	@FXML
+	public void deconnexion() {
+		managerGui.showView(EnumView.Connexion);
+	}
 }
