6d4c5429bbb8bb35bea5ad887bc9053493c1db08
bill williams <tsofackb@3il.fr>
2020-06-07 13:10:40 +0200

    droit d'accès epreuve et tache

--- src/projet/view/MenuController.java
+++ src/projet/view/MenuController.java
@@ -7,14 +7,20 @@ import java.net.URISyntaxException;
 
 import javax.inject.Inject;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import jfox.javafx.view.IManagerGui;
+import projet.commun.Roles;
+import projet.data.Utilisateur;
 import projet.view.connexion.ModelConnexion;
 
 public class MenuController {
 
 	@Inject
 	private IManagerGui managerGui;
+	
+	@Inject
+	private ModelConnexion modelConnexion;
 
 	@FXML
 	public void doAccueil() {
@@ -70,10 +76,42 @@ public class MenuController {
 	
 	@FXML
 	public void doEpreuveAdmin() {
-		managerGui.showView(EnumView.EpreuveAdmin);
+		configurerMenu(modelConnexion.getUtilisateurActif(), 0);
+		// le changement du compte connecté modifie automatiquement le menu
+		modelConnexion.UtilisateurActifProperty().addListener((obs) -> {
+			Platform.runLater( () -> configurerMenu( modelConnexion.getUtilisateurActif(), 0 ) );
+		});
 	}
 	@FXML
 	public void doCategorie() {
-		managerGui.showView(EnumView.Tache);
+		configurerMenu(modelConnexion.getUtilisateurActif(), 1);
+		// le changement du compte connecté modifie automatiquement le menu
+		modelConnexion.UtilisateurActifProperty().addListener((obs) -> {
+			Platform.runLater( () -> configurerMenu( modelConnexion.getUtilisateurActif(), 0 ) );
+		});
 	}
+	
+	
+	private void configurerMenu( Utilisateur utilisateurActif, int categorie ) {
+		if( utilisateurActif != null ) {
+			if (categorie == 0) { // si c'est une épreuve
+				if( utilisateurActif.isInRole( Roles.UTILISATEUR) ) {
+					managerGui.showDialogMessage("Désolé seul les administrateurs ont accès à ces données");
+				}
+				if( utilisateurActif.isInRole( Roles.ADMINISTRATEUR ) ) {
+					managerGui.showView(EnumView.EpreuveAdmin);
+				}
+			}
+			else { // si c'est une tache
+				if( utilisateurActif.isInRole( Roles.UTILISATEUR) ) {
+					managerGui.showDialogMessage("Désolé seul les administrateurs ont accès à ces données");
+				}
+				if( utilisateurActif.isInRole( Roles.ADMINISTRATEUR ) ) {
+					managerGui.showView(EnumView.Tache);
+				}
+			}
+			
+		}
+	}
+	
 }
